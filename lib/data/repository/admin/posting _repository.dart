f87be683d5819629fb2e10f03dfee311fcd6d89a
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2025-06-05 10:16:17 +0700

    Membuat posting_repository

---
+++ lib/data/repository/admin/posting _repository.dart
@@ -0,0 +1,52 @@
+import 'dart:convert';
+import 'dart:developer';
+import 'package:dartz/dartz.dart';
+import 'package:ternak_kenari_app/data/model/request/admin/posting_jual_request_model.dart';
+import 'package:ternak_kenari_app/data/model/response/burung_semua_tersedia_model.dart';
+import 'package:ternak_kenari_app/data/model/response/get_all_burung_response_model.dart';
+import 'package:ternak_kenari_app/service/service_http_client.dart';
+
+class PostingRepository {
+  final ServiceHttpClient _serviceHttpClient;
+  PostingRepository(this._serviceHttpClient);
+
+  Future<Either<String, BurungSemuaTersediabyIdModel>> addPotBurung(
+    Postingjualrequestmodel requestModel,
+  ) async {
+    try {
+      final response = await _serviceHttpClient.postWithToken(
+        'admin/posting-jual',
+        requestModel.toMap(),
+      );
+      final jsonResponse = json.decode(response.body);
+      if (response.statusCode == 201) {
+        final profileResponse = BurungSemuaTersediabyIdModel.fromJson(
+          jsonResponse,
+        );
+        log("Add Burung successful: ${profileResponse.message}");
+        return Right(profileResponse);
+      } else {
+        log("Add burung failed: ${jsonResponse['message']}");
+        return Left(jsonResponse['message'] ?? "Add burung failed");
+      }
+    } catch (e) {
+      return Left("Error in Add burung: $e");
+    }
+  }
+
+  Future<Either<String, GetAllBurungModel>> getAllBurung() async {
+    try {
+      final response = await _serviceHttpClient.get("admin/burung-semua");
+      if (response.statusCode == 200) {
+        final jsonResponse = json.decode(response.body);
+        final getAllBurung = GetAllBurungModel.fromMap(jsonResponse);
+        return Right(getAllBurung);
+      } else {
+        final jsonResponse = json.decode(response.body);
+        return Left(jsonResponse['message'] ?? "Get All burung failed");
+      }
+    } catch (e) {
+      return Left("Error in getting all burung: $e");
+    }
+  }
+}
