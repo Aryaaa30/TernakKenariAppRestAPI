024c36e77ca1c1783d16127cbb6b7c28bd5a8424
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2025-06-05 11:48:03 +0700

    Membuat profile_view_buyer

---
+++ lib/presentation/buyer/profile/widget/profile_view_buyer.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:ternak_kenari_app/core/core.dart';
+import 'package:ternak_kenari_app/data/model/response/buyer/buyer_profile_response_model.dart';
+
+class ProfileViewBuyer extends StatelessWidget {
+  final Data profile;
+  const ProfileViewBuyer({super.key, required this.profile});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "Nama: ${profile.name}",
+            style: TextStyle(
+              fontSize: MediaQuery.of(context).size.width * 0.05,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(
+            "Alamat: ${profile.address}",
+            style: TextStyle(
+              fontSize: MediaQuery.of(context).size.width * 0.04,
+            ),
+          ),
+          Text(
+            "No HP: ${profile.phone}",
+            style: TextStyle(
+              fontSize: MediaQuery.of(context).size.width * 0.04,
+            ),
+          ),
+          const SizedBox(height: 16),
+          TextButton(
+            onPressed: () {
+              context.pushAndRemoveUntil(
+                const BuyerHomeScreen(),
+                (route) => false,
+              );
+            },
+            child: const Text("Go to Home"),
+          ),
+        ],
+      ),
+    );
+  }
+}
