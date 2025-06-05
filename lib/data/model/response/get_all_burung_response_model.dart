6ccc56bcf4b7ed7ca6b1c5ac8057748a5d39e0df
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2025-06-05 08:32:46 +0700

    Menamahkan model GetAllBurung

--- lib/data/model/response/get_all_burung_response_model.dart
+++ lib/data/model/response/get_all_burung_response_model.dart
@@ -20,4 +20,24 @@ class GetAllBurungModel {
     statusCode: statusCode ?? this.statusCode,
     data: data ?? this.data,
   );
+
+  factory GetAllBurungModel.fromJson(String str) =>
+      GetAllBurungModel.fromMap(json.decode(str));
+
+  String toJson() => json.encode(toMap());
+
+  factory GetAllBurungModel.fromMap(Map<String, dynamic> json) =>
+      GetAllBurungModel(
+        message: json["message"],
+        statusCode: json["status_code"],
+        data: List<GetAllBurung>.from(
+          json["data"].map((x) => GetAllBurung.fromMap(x)),
+        ),
+      );
+
+  Map<String, dynamic> toMap() => {
+    "message": message,
+    "status_code": statusCode,
+    "data": List<dynamic>.from(data.map((x) => x.toMap())),
+  };
 }
