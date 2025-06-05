142b1b3abbd2ca0e12cc6d13d3e0ad4c012150e5
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2025-06-05 08:06:37 +0700

    Membuat admin_profile_response_model

---
+++ lib/data/model/response/admin_profile_response_model.dart
@@ -0,0 +1,9 @@
+import 'dart:convert';
+
+class AdminProfileResponseModel {
+  final String? message;
+  final int? statusCode;
+  final AdminProfile? data;
+
+  AdminProfileResponseModel({this.message, this.statusCode, this.data});
+}
