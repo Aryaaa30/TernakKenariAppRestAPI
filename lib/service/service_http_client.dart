2c38969b7e8a1490672938137355ff4bdd4b368b
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2025-06-05 09:58:25 +0700

    Membuat profile_admin_repository dan method postWithToken

--- lib/service/service_http_client.dart
+++ lib/service/service_http_client.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
 
@@ -7,7 +6,7 @@ class ServiceHttpClient {
   final String baseUrl = 'http://10.0.0.2:8000/api/';
   final secureStorage = FlutterSecureStorage();
 
-  //POST
+  // POST tanpa token
   Future<http.Response> post(String endPoint, Map<String, dynamic> body) async {
     final url = Uri.parse("$baseUrl$endPoint");
     try {
@@ -25,7 +24,29 @@ class ServiceHttpClient {
     }
   }
 
-  //GET
+  Future<http.Response> postWithToken(
+    String endPoint,
+    Map<String, dynamic> body,
+  ) async {
+    final token = await secureStorage.read(key: "token");
+    final url = Uri.parse("$baseUrl$endPoint");
+    try {
+      final response = await http.post(
+        url,
+        headers: {
+          if (token != null) 'Authorization': 'Bearer $token',
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: jsonEncode(body),
+      );
+      return response;
+    } catch (e) {
+      throw Exception("POST with token request failed: $e");
+    }
+  }
+
+  // GET dengan token
   Future<http.Response> get(String endPoint) async {
     final token = await secureStorage.read(key: "token");
     final url = Uri.parse("$baseUrl$endPoint");
@@ -44,6 +65,5 @@ class ServiceHttpClient {
     }
   }
 
-  //PUT
-  //DELETE
+  // TODO: Tambahkan method PUT & DELETE jika dibutuhkan nanti
 }
