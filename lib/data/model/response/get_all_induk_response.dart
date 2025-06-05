219c9e2287a8b6e1d359e7fd1dcb3b3ffa92909d
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2025-06-05 09:00:32 +0700

    Menambahkan metode copyWith dan serialisasi JSON di GetIndukById

--- lib/data/model/response/get_all_induk_response.dart
+++ lib/data/model/response/get_all_induk_response.dart
@@ -10,4 +10,28 @@ class GetIndukById {
     required this.statusCode,
     required this.data,
   });
+
+  GetIndukById copyWith({String? message, int? statusCode, GetInduk? data}) =>
+      GetIndukById(
+        message: message ?? this.message,
+        statusCode: statusCode ?? this.statusCode,
+        data: data ?? this.data,
+      );
+
+  factory GetIndukById.fromRawJson(String str) =>
+      GetIndukById.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory GetIndukById.fromJson(Map<String, dynamic> json) => GetIndukById(
+    message: json["message"],
+    statusCode: json["status_code"],
+    data: GetInduk.fromJson(json["data"]),
+  );
+
+  Map<String, dynamic> toJson() => {
+    "message": message,
+    "status_code": statusCode,
+    "data": data.toJson(),
+  };
 }
